6754aae7cb9cf172142381d56046d920f5d9e7db
vesrozeno <vtr.rozeno@gmail.com>
2024-04-27 12:04:13 -0300

    first commit

--- android/app/src/main/java/android/pkg/MainActivity.kt
+++ android/app/src/main/java/android/pkg/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.ticketsappreactnative
+package android.pkg
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/android/pkg/MainApplication.kt
+++ android/app/src/main/java/android/pkg/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.ticketsappreactnative
+package android.pkg
 
 import android.app.Application
 import android.content.res.Configuration
